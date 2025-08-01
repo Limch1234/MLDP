34f4b1c815856ea2b6ed93489b0462d34ed1edd1
Limch1234 <limchinhuan433@gmail.com>
2025-08-01 13:44:48 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -30,6 +30,14 @@ mdata = joblib.load('best_gradient_boosting_model.pkl') # Load the model and fea
 model = mdata['gbr_model'] # Load the model
 feature_columns = mdata['feature_columns'] # Load the feature columns
 # Define the features used in the model
+
+backgrounds = {
+    'Dry': 'https://thumbs.dreamstime.com/b/singapore-september-close-up-marina-bay-sands-wonderful-cityscape-sunny-day-shot-three-towers-ressort-against-135571833.jpg',
+    'Light Rain': 'https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-1910724454.jpg?c=original',
+    'Moderate Rain': 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0ddc54f5-5910-407a-8384-a82c5ef44c24/dcv1iia-008c77fc-837e-488b-8c5d-165013a2a95d.jpg/v1/fill/w_1280,h_854,q_75,strp/light_rain_heavy__traffic_singapore_sunset_by_capturing_the_light_dcv1iia-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODU0IiwicGF0aCI6IlwvZlwvMGRkYzU0ZjUtNTkxMC00MDdhLTgzODQtYTgyYzVlZjQ0YzI0XC9kY3YxaWlhLTAwOGM3N2ZjLTgzN2UtNDg4Yi04YzVkLTE2NTAxM2EyYTk1ZC5qcGciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.hb4uom4x_5FZtami28_lJREWjIhzatms_kvC_4vkoqw',
+    'Heavy Rain': 'https://finbarrfallon.com/wp-content/uploads/2021/06/Storm_FinbarrFallon2-1000x667.jpg'
+}
+
 all_features = ['windspeed', 'temperature', 'humidity', 'wind_direction_East', 'wind_direction_North',
                 'wind_direction_North-East', 'wind_direction_North-West', 'wind_direction_South-East',
                 'wind_direction_South-West', 'month_April', 'month_August', 'month_December',
@@ -87,5 +95,26 @@ if st.button('Predict'):
         rain_class = "Heavy Rain"
         st.warning("Heavy rainfall predicted! Please take necessary precautions.")
 
-    st.success(f'The predicted average rainfall is: {y_pred:.2f} mm'
-               f' ({rain_class})') # Display the predicted average rainfall
+
+    url_background = backgrounds[rain_class] # Get the background image URL based on the predicted rainfall class
+    st.markdown(f"""
+        <style>
+        .stApp {{
+            background-image: url("{url_background}");
+            background-size: cover;
+            background-attachment: fixed;
+            background-repeat: no-repeat;
+        }}
+        .stApp::before {{
+            content: "";
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.5); /* White overlay with transparency */
+            z-index: -1; /* Ensure the overlay is behind the content */
+        }}
+    </style>
+    """, unsafe_allow_html=True)
+st.success(f'The predicted average rainfall is: {y_pred:.2f} mm ({rain_class})') # Display the predicted average rainfall
